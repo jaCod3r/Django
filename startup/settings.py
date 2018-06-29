fb5565190a7869de80dd8f1b9d3f73bf65925a20
jaCod3r <a.juber@hotmail.com>
2018-06-29 17:55:50 -0400

    Changed db

--- startup/settings.py
+++ startup/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(default='postgres://...')
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
