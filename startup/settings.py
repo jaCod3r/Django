d1744f923914d0d02e57e29a2c8867ec86c691c0
jaCod3r <a.juber@hotmail.com>
2018-06-29 17:02:10 -0400

    Updated my settings

--- startup/settings.py
+++ startup/settings.py
@@ -76,15 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'startup.wsgi.application'
 
-STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, "static"),
-        ]
-        STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-        STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
