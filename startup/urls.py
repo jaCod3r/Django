4b4174cd016beb09827f856b97216834e0d50d45
jaCod3r <a.juber@hotmail.com>
2018-06-29 13:19:30 -0400

    Small change

--- startup/urls.py
+++ startup/urls.py
@@ -1,5 +1,5 @@
 """startup URL Configuration
-
+small change
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
